a088077332735aca7f41bab1f55d103dfb8a703f
KostasAnag <82450466+KostasAnag@users.noreply.github.com>
2023-07-03 22:24:50 +0300

    typos fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from "react";
 
-export default function About(){
-    return(
+export default function About() {
+  return (
     <div className="about-wrapper">
-        <h4>About</h4>
-        <p>I am a junior frontend developer with a scientific background in physics.I try to keep up with latest technologies and best practises in order to make my code simple , readable and efficient. I really like learning new things constantly and evolving as a proffesional and as a person in general .</p>
+      <h4>About</h4>
+      <p>
+        I am a junior frontend developer with a scientific background in
+        physics.I try to keep up with latest technologies and best practices in
+        order to make my code simple , readable and efficient. I really like
+        learning new things constantly and evolving as a professional and as a
+        person in general .
+      </p>
     </div>
-    )
+  );
 }
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,18 +1,23 @@
-import avatar from './images/laptop-photo.jpg'
-import React from 'react'
-import envelope from './images/Icon.png'
-import linkedIn from './images/linkedin.png'
- export default function Info(){
-    return (
-        <div className= "info-wrapper">
-        <img src= {avatar} className= "avatar"/>
-        <h2>Kostas Anagnostou</h2>
-        <span>Frontend Developer</span>
-        <p>kostasAnag.website</p>
-         <div className='btn-wrapper'>
-         <button className='mail-btn'><img src={envelope} /> Email</button> 
-        <button className='linked-btn'><img src={linkedIn} /> LinkedIn</button>
-        </div>
-        </div>
-    )
+import avatar from "./images/laptop-photo.jpg";
+import React from "react";
+import envelope from "./images/Icon.png";
+import linkedIn from "./images/linkedin.png";
+
+export default function Info() {
+  return (
+    <div className="info-wrapper">
+      <img src={avatar} className="avatar" />
+      <h2>Kostas Anagnostou</h2>
+      <span>Frontend Developer</span>
+      <p>kostasAnag.website</p>
+      <div className="btn-wrapper">
+        <button className="mail-btn">
+          <img src={envelope} /> Email
+        </button>
+        <button className="linked-btn">
+          <img src={linkedIn} /> LinkedIn
+        </button>
+      </div>
+    </div>
+  );
 }
